a6b2140df2ede18a8850c3b4f34e1b353b41a4d4
glozex <kalinkageorgy@gmail.com>
2019-12-20 23:29:13 +0800

    feat(blog.html): Added a new page
    feat(_custom.js): Added mobile version of Mmenu
    feat(_media.sass): Added the file to edit the media queries
    chore(index.html): Added links. Added support for phones
    chore(main.sass): Added new styles for the blog.html
    chore(package.json): Removed libraries
    fix(main.sass): Fixed the background

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,13 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var menu = new Mmenu("#my-menu", {
-		extensions: ["theme-black", "effect-menu-slide", "pagedim-black", "position-right"],
-		navbar: {
-			title: '<img src="img/_src/logo-mmenu.svg" alt="RAIDEMOT">'
-		},
-	});
+	var window_size = window.matchMedia("(max-width: 768px)");
+
+	if (window_size.matches) {
+		var menu = new Mmenu("#my-menu", {
+			extensions: ["theme-black", "effect-menu-slide", "pagedim-black", "position-right", "fullscreen"],
+			navbar: {
+				title: '<img id="logoPanelMmenu" src="img/_src/logo-mmenu.svg" alt="RAIDEMOT">'
+			},
+		});
+	} else
+	{
+		var menu = new Mmenu("#my-menu", {
+			extensions: ["theme-black", "effect-menu-slide", "pagedim-black", "position-right"],
+			navbar: {
+				title: '<img id="logoPanelMmenu" src="img/_src/logo-mmenu.svg" alt="RAIDEMOT">'
+			},
+		});
+	}
 
 	var apiMmenu = menu.API;
 	var hamburger = document.getElementsByClassName('hamburger')[0];
+	var logoPanelMmenu = document.getElementById('logoPanelMmenu');
+
+	logoPanelMmenu.onclick = function(event) {
+		event = event || window.event;
+		apiMmenu.close();  
+	};
+
+	hamburger.onclick = function(event) {
+		event = event || window.event;
+		apiMmenu.close();  
+	};
 
 	apiMmenu.bind("open:finish",
 		() => {
